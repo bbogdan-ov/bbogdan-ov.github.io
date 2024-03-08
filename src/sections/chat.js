76b3f0257d58014de1a36d2042f3ceab78eba582
bogdanov <bbogdan_ov@mail.ru>
2024-03-08 19:46:25 +0300

    ask for password once

--- src/sections/chat.js
+++ src/sections/chat.js
@@ -2,6 +2,7 @@ import * as funny from "../funny";
 import * as vault from "../sections/vault";
 
 const giveMePasswordPlease = ["password please", "password pls", "give me password right now"]
+let isAlreadySaid = false;
 
 export function initChat() {
     const input = document.querySelector("#chat-input");
@@ -10,7 +11,12 @@ export function initChat() {
     button.onclick = ()=> {
         // Alert password if user asks
         if (giveMePasswordPlease.includes(input.value.toLowerCase())) {
-            alert(`the password is ${ vault.vaultPassword }, but DO NOT TELL ANYONE`);
+            if (!isAlreadySaid) {
+                alert(`the password is ${ vault.vaultPassword }, but DO NOT TELL ANYONE`);
+                isAlreadySaid = true;
+            } else {
+                alert("I'VE ALREADY GIVEN YOU THE PASSWORD, GO AWAY!")
+            }
             return;
         }
         
