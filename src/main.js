f6a8d1273698f163166bb355edd4a7a7a9d2c935
bogdanov <bbogdan_ov@mail.ru>
2024-03-08 19:08:30 +0300

    create `funny.js` and move some funcs from `main.js`

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import * as speedup from "./speedup";
 import * as utils from "./utils";
 import * as games from "./games";
 import * as loading from "./loading";
+import * as funny from "./funny";
 
 // Pixilate textures
 PIXI.BaseTexture.defaultOptions.scaleMode = PIXI.SCALE_MODES.NEAREST;
@@ -30,43 +31,6 @@ document.addEventListener("DOMContentLoaded", ()=> {
     })
 })
 
-// Fun stuff
-function playGoofySounds() {
-    utils.playSound(utils.randomItem(["tada", "nerd", "cola", "spring", "slide", "yell", "scream", "sad"]), .5);
-}
-/** @param el {HTMLElement}  */
-export function animateFlyout(el) {
-    if (typeof el == "string")
-        el = document.querySelector(el);
-
-    if (el.classList.contains("animating"))
-        return;
-
-    el.classList.add("animating");
-    utils.playSound("slide", .3); 
-
-    const frames = [
-        {
-            opacity: 0,
-            transform: "rotateZ(-270deg) rotateY(360deg) translate(-100%, 300%)"
-        },
-        {
-            opacity: 1
-        },
-        {
-            opacity: 1,
-            transform: "rotateZ(0deg) rotateY(0deg) translate(0%, 0)"
-        },
-    ]
-    const opts = { duration: 5000, easing: "ease-out", fill: "forwards" };
-    
-    el.style.pointerEvents = "unset";
-    el.animate(frames, opts).onfinish = ()=> {
-        el.classList.remove("animating");
-        utils.playSound("tada", .5);
-    }
-}
-
 window.global_playSound = utils.playSound;
-window.global_playGoofySounds = playGoofySounds;
-window.global_animateFlyout = animateFlyout;
+window.global_playGoofySounds = funny.playGoofySounds;
+window.global_animateFlyout = funny.animateFlyout;
