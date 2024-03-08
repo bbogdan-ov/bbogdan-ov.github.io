3debc3edc743295501ab973039b87c2b1338a74f
bogdanov <bbogdan_ov@mail.ru>
2024-03-08 19:09:11 +0300

    add alerting the password if user asks

--- src/sections/chat.js
+++ src/sections/chat.js
@@ -1,3 +1,20 @@
+import * as funny from "../funny";
+import * as vault from "../sections/vault";
+
+const giveMePasswordPlease = ["password please", "password pls", "give me password right now"]
+
 export function initChat() {
-    
+    const input = document.querySelector("#chat-input");
+    const button = document.querySelector("#chat-button");
+
+    button.onclick = ()=> {
+        // Alert password if user asks
+        if (giveMePasswordPlease.includes(input.value.toLowerCase())) {
+            alert(`the password is ${ vault.vaultPassword }, but DO NOT TELL ANYONE`);
+            return;
+        }
+        
+        // Otherwise play LOVELY animation
+        funny.animateFlyout("#lovelove");
+    }
 }
