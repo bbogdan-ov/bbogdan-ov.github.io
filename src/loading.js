af05b919a911bb4877e66915cee55cd13abc9232
bogdanov <bbogdan_ov@mail.ru>
2024-07-12 00:19:29 +0300

    v1.3.1 a few optimizations

--- src/loading.js
+++ src/loading.js
@@ -1,25 +1,22 @@
-import * as utils from "./utils";
-
 export function initLoading() {
-    const screen = document.querySelector(".loading-screen");
+	const screen = document.querySelector(".loading-screen");
 
-    // Remove loading screen in dev mode
-    if (import.meta.env.DEV) {
-        screen.remove();
-        return;
-    }
-    
-    // Lock scrolling
-    document.body.style.overflowY = "hidden";
-    scrollTo(0, 0);
-    
-    addEventListener("load", ()=> {
-        screen.classList.add("loaded");
-        utils.playSound("spring", .8);
+	// Remove loading screen in dev mode
+	if (import.meta.env.DEV) {
+		screen.remove();
+		return;
+	}
 
-        // Free scrolling after 3 secs
-        setTimeout(()=> {
-            document.body.style.overflowY = "unset";
-        }, 3500);
-    });
+	// Lock scrolling
+	document.body.style.overflowY = "hidden";
+	scrollTo(0, 0);
+
+	addEventListener("load", ()=> {
+		screen.classList.add("loaded");
+
+		// Free scrolling after 3 secs
+		setTimeout(()=> {
+			document.body.style.overflowY = "unset";
+		}, 3500);
+	});
 }
