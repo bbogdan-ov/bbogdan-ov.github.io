a4a1b131c8504357485ed551da9b03af91fc71d2
bogdanov <bbogdan_ov@mail.ru>
2024-11-28 03:14:47 +0000

    simplify cookies code

--- src/sections/cookies.js
+++ src/sections/cookies.js
@@ -1,64 +1,48 @@
+import gsap from "gsap";
+
 export function initCookies() {
-	const card = document.querySelector(".fuck-cookies");
-	const closeButton = card.querySelector(".close-button");
-	const allowButtons = card.querySelector(".allow-buttons").children;
+    const card = document.querySelector(".fuck-cookies");
+    const closeButton = card.querySelector(".close-button");
+    const allowButtons = card.querySelector(".allow-buttons").children;
 
-	for (const btn of allowButtons) {
-		btn.onclick = handleAllowClick;
-	}
+    for (const btn of allowButtons) {
+        btn.onclick = handleAllowClick;
+    }
 
-	closeButton.onclick = e=> {
-		e.preventDefault();
-		e.stopPropagation();
+    closeButton.onclick = e=> {
+        e.preventDefault();
+        e.stopPropagation();
 
-		closeButton.classList.add("fly-away");
+        closeButton.classList.add("fly-away");
 
-		closeButton.animate([
-			{ transform: "translateY(0px)", easing: "ease-in-out" },
-			{ transform: "translateY(-10px)", easing: "ease-in-out" },
-			{ transform: "translateY(10px)", easing: "ease-in-out" },
-			{ transform: "translateY(-10px)", easing: "ease-in-out" },
-			{ transform: "translateY(10px)", easing: "ease-in-out" },
-			{ transform: "translateY(-10px)", easing: "ease-in-out" },
-			{ transform: "translateY(10px)", easing: "ease-in-out" },
-			{ transform: "translateY(-10px)", easing: "ease-in-out" },
-			{ transform: "translateY(10px)", easing: "ease-in-out" },
-			{ transform: "translateY(-10px)", easing: "ease-in-out" },
-			{ transform: "translateY(10px)", easing: "ease-in-out" },
-			{ transform: "translateY(-10px)", easing: "ease-in-out" },
-			{ transform: "translateY(10px)", easing: "ease-in-out" },
-		], {
-			duration: 10000,
-			delay: 2000,
-			fill: "both"
-		})
-		closeButton.animate([
-			{ transform: `translateX(${ -innerWidth }px)` },
-		], {
-			duration: 10000,
-			delay: 2000,
-			easing: "linear",
-			fill: "both"
-		});
-	}
+        gsap.to(closeButton, {
+            keyframes: {
+                y: [ 0, 20, 0 ],
+                easeEach: "power1.inOut",
+            },
+            duration: 3,
+            delay: 2,
+            repeat: -1,
+        })
+        gsap.to(closeButton, {
+            x: -innerWidth - 100,
+            duration: 10,
+            delay: 2,
+            ease: "none",
+            onComplete: () => {
+                closeButton.remove();
+            }
+        })
 
-	function handleAllowClick() {
-		const oldBounds = card.getBoundingClientRect();
+    }
 
-		card.style.textAlign = "center";
-		card.innerHTML = `
-			<div class="cookies-marquee"></div>
-			<h3>* THANKS *</h3>
-			<p>Now i will show you ads based on your activity in the internet!</p>
-			<div class="cookies-marquee"></div>
-		`;
-
-		const bounds = card.getBoundingClientRect();
-
-		card.style.height = oldBounds.height + "px";
-		card.animate(
-			{ height: bounds.height + "px" },
-			{ duration: 2000, easing: "linear", delay: 500, fill: "both" }
-		);
-	}
+    function handleAllowClick() {
+        card.style.textAlign = "center";
+        card.innerHTML = `
+            <div class="cookies-marquee reverse-anim"></div>
+            <h3>* THANKS *</h3>
+            <p>Now i will show you ads based on your activity in the internet!</p>
+            <div class="cookies-marquee"></div>
+        `;
+    }
 }
