24c06a84aff229d4e1c583ac8a0b6f0939787c6c
bogdanov <bbogdan_ov@mail.ru>
2024-11-30 14:34:52 +0000

    remove useless comments

--- src/utils.js
+++ src/utils.js
@@ -1,47 +1,4 @@
 import * as consts from "./consts";
-//import click_audio from "/audios/click.mp3";
-//import cola_audio from "/audios/cola.mp3";
-//import goofyThrow1_audio from "/audios/goofy-throw-1.mp3";
-//import goofyThrow2_audio from "/audios/goofy-throw-2.mp3";
-//import nerd_audio from "/audios/nerd.mp3";
-//import raaah_audio from "/audios/raaah.mp3";
-//import sad_audio from "/audios/sad.mp3";
-//import scream_audio from "/audios/scream.mp3";
-//import slide_audio from "/audios/slide.mp3";
-//import spring_audio from "/audios/spring.mp3";
-//import tada_audio from "/audios/tada.mp3";
-//import tear1_audio from "/audios/tear-1.mp3";
-//import tear2_audio from "/audios/tear-2.mp3";
-//import throw1_audio from "/audios/throw-1.mp3";
-//import throw2_audio from "/audios/throw-2.mp3";
-//import yell_audio from "/audios/yell.mp3";
-//import wrong_audio from "/audios/wrong.mp3";
-//import correct_audio from "/audios/correct.mp3";
-//import knock_audio from "/audios/knock.mp3";
-//import meow_audio from "/audios/meow.mp3";
-
-//export const AUDIO_NAMES = {
-//    "click": click_audio,
-//    "cola": cola_audio,
-//    "goofy-throw-1": goofyThrow1_audio,
-//    "goofy-throw-2": goofyThrow2_audio,
-//    "nerd": nerd_audio,
-//    "raaah": raaah_audio,
-//    "sad": sad_audio,
-//    "scream": scream_audio,
-//    "slide": slide_audio,
-//    "spring": spring_audio,
-//    "tada": tada_audio,
-//    "tear-1": tear1_audio,
-//    "tear-2": tear2_audio,
-//    "throw-1": throw1_audio,
-//    "throw-2": throw2_audio,
-//    "yell": yell_audio,
-//    "wrong": wrong_audio,
-//    "correct": correct_audio,
-//    "knock": knock_audio,
-//    "meow": meow_audio,
-//}
 
 export function remap(alpha, from, to) {
     return alpha * (to - from) + from;
