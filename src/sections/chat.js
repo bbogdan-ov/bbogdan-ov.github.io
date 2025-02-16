cb46bb161d626fd5422827fae6cdae2773d0a8c9
bogdanov <bbogdan_ov@mail.ru>
2025-02-16 22:38:26 +0000

    add chat with god

--- src/sections/chat.js
+++ src/sections/chat.js
@@ -1,28 +1,98 @@
-import * as funny from "../funny";
-import * as vault from "../sections/vault";
+import * as utils from "../utils";
 
-const giveMePasswordPlease = ["password please", "password pls", "give me password right now"]
-let isAlreadySaid = false;
+const ANSWERS = [
+	"shut your bitch ass",
+	"hi",
+	"no",
+	"yes",
+	"what do you want from me???",
+	"get out!",
+	"beeeep boooop aa",
+	"computers will make humanity lazy as fat dogs",
+	"IM YET ANOTHER AI ASSISTANT",
+	"uuuh, ok",
+	"erm",
+	"let me die",
+	"goodbye",
+	"hey",
+	"holy moly",
+	"i dont think i understood you correctly",
+	"sacrifice the baby",
+	"gay is illness",
+	"stay tuned",
+	"THE NATIONAL ATHEM is one of the best things ive ever heard",
+	"STARING A HOLE IN THE FLOOR is one of the best things ive ever heard",
+	"I'LL KEEP COMING is one of the best things ive ever heard",
+	"PLEADER is one of the best things ive ever heard",
+	"BLESSED ACT is one of the best things ive ever heard",
+	"i hear nothing",
+	"well...",
+	"please shut up",
+	"let me sleep",
+	"god damn",
+	"talking to a fkn computer is better than talking to a real human, huh?",
+	"yeah, i just spam random phrases",
+	"uuum, eeeem",
+	"shshshsh",
+	"--.-.-.--..--..-.-.-...--",
+	"that means nothing!",
+	"you number is 5",
+	"shut your bitch ass again",
+];
 
 export function initChat() {
 	const chat = document.querySelector(".chat");
+	const messages = chat.querySelector(".messages");
+	const printingMsg = chat.querySelector(".printing-msg");
+	/** @type {HTMLInputElement} */
 	const input = chat.querySelector(".chat-input");
-	const button = chat.querySelector(".chat-button");
-	const lovelove = chat.querySelector(".lovelove");
+	/** @type {HTMLFormElement} */
+	const form = chat.querySelector(".chat-form");
 
-	button.onclick = () => {
-		// Alert password if user asks
-		if (giveMePasswordPlease.includes(input.value.toLowerCase())) {
-			if (!isAlreadySaid) {
-				alert(`the password is ${vault.password}, but DO NOT TELL ANYONE`);
-				isAlreadySaid = true;
-			} else {
-				alert("I'VE ALREADY GIVEN YOU THE PASSWORD, GO AWAY!")
-			}
-			return;
+	form.onsubmit = e => {
+		e.preventDefault();
+		if (input.value == "") return;
+
+		addMessage("you", "#222", input.value);
+		input.value = "";
+
+		form.inert = true;
+		printingMsg.style.display = "initial";
+
+		setTimeout(() => {
+			addMessage("god", "#f00", utils.randomItem(ANSWERS), true);
+
+			form.inert = false;
+			input.focus();
+			printingMsg.style.display = "none";
+		}, 2000);
+	}
+
+	function addMessage(author, color, text, textAnim = false) {
+		const element = document.createElement("div");
+		element.className = "message";
+		element.innerHTML = `
+			<b style="color: ${ color };">${ author }:</b>
+		`;
+
+		const textEl = document.createElement("span");
+		element.appendChild(textEl);
+		if (!textAnim)
+			textEl.textContent = text;
+
+		if (textAnim) {
+			let charIndex = 0;
+			const interval = setInterval(() => {
+				if (charIndex >= text.length) {
+					clearInterval(interval);
+					return;
+				}
+
+				textEl.textContent += text[charIndex];
+				charIndex += 1;
+			}, 20);
 		}
 
-		// Otherwise play LOVELY animation
-		funny.animateFlyout(lovelove);
+		messages.appendChild(element);
 	}
 }
